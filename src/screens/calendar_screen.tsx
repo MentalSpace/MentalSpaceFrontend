77fcb376d0e8884ec639cac385d9bc93f9bc080e
nixalpha <n1xalphaa@gmail.com>
2022-06-28 20:05:55 -0700

    Add the top bar of the new calendar screen Removed the old broken calendar and also integrated styled-components for future use

--- src/screens/calendar_screen.tsx
+++ src/screens/calendar_screen.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { Button, View } from 'react-native';
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SideBarList } from "../components/main_stack";
-import AgendaScreen from "./agendaScreen";
+import { useTheme } from "native-base";
+
+import CalendarStrip from 'react-native-calendar-strip';
 
 type CalendarScreenProps = NativeStackScreenProps<SideBarList, 'Calendar'>;
 
 function CalendarScreen({ navigation }: CalendarScreenProps) {
+  const theme = useTheme();
+
   return (
-    <AgendaScreen />
+    <CalendarStrip 
+      style={{
+        height: 150,
+        paddingTop: 20
+      }}
+      highlightDateNumberStyle={{
+        color: '#fff',
+        backgroundColor: theme.colors.primary[500],
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: "flex"
+      }}
+      highlightDateNameStyle={{color: theme.colors.primary[700]}}
+    />
   );
 }
 
