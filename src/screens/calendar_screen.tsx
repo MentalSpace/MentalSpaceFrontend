d76e6f13e79fea6599214418a1ffceecc929ac73
vinngo <104804772+vinngo@users.noreply.github.com>
2022-06-30 09:34:21 -0700

    Update calendar_screen.tsx
    removes css file

--- src/screens/calendar_screen.tsx
+++ src/screens/calendar_screen.tsx
@@ -1,66 +1,51 @@
-import React from "react";
-import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack";
-import { SideBarList } from "../components/main_stack";
-import { Box, HamburgerIcon, Menu, Pressable, useTheme, View } from "native-base";
-import CalendarStrip from 'react-native-calendar-strip';
+import React from 'react';
+import { Box, Button, Center, FormControl, Heading, Select, VStack } from "native-base";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {SideBarList} from '../components/main_stack'
 
-type CalendarScreenProps = NativeStackScreenProps<SideBarList, 'Calendar'>;
+type SubscribeMenuProps = NativeStackScreenProps<SideBarList, "SubscribeMenu">
 
-function CalendarScreen({ navigation }: CalendarScreenProps) {
-  const theme = useTheme();
-
+function SubscribeMenu ({navigation}: SubscribeMenuProps){
   return (
-    <Box flex={1}>
-      <CalendarStrip
-        calendarHeaderStyle={{
-          fontFamily: theme.fonts.heading, 
-          fontSize: theme.fontSizes.lg
-        }}
-        dateNumberStyle={{fontSize: theme.fontSizes.xl}}
-        style={{
-          height: 150,
-          paddingTop: 20
-        }}
-        highlightDateNumberStyle={{
-          color: '#fff',
-          backgroundColor: theme.colors.primary[500],
-          height: 40, 
-          width: 40,
-          borderRadius: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-          // hacks to make the circle and text consistent on both mobile and web
-          fontSize: theme.fontSizes.xl,
-          paddingTop: 5
-        }}
-        highlightDateNameStyle={{color: theme.colors.primary[700]}}
-        iconContainer={{ flex: 0.15 }}
-      />
-      <Menu trigger={triggerProps => {
-        return (
-          <Pressable 
-            position="absolute" 
-            height="60" 
-            width="60" 
-            borderRadius="30" 
-            bottom='30'
-            right='30'
-            justifyContent="center"
-            alignItems="center"
-            backgroundColor={theme.colors.primary[600]} 
-            accessibilityLabel="More options menu" {...triggerProps}
-          >
-              <HamburgerIcon color='#fff'/>
-          </Pressable>);
-      }}>
-        <Menu.Item onPress={() => navigation.navigate('SubscribeMenu')} >Subscribe to Classes</Menu.Item>
-        <Menu.Item>See Assignments</Menu.Item>
-        <Menu.Item>See and Edit Schedule</Menu.Item>
-        <Menu.Item>Edit Homework Priorities</Menu.Item>
-        <Menu.Item>Edit non-homework activities</Menu.Item>
-      </Menu>
+    <Center w = "100%">
+    <Box safeArea p="2" w="90%" maxW="290" py="8">
+      <VStack space={3} mt="5">
+      <Heading size="lg" color="coolGray.800" _dark={{
+        color: "warmGray.50"
+      }} fontWeight="semibold">
+          Subscribe To...
+      </Heading>
+      <Heading size = "md" color = "coolGray.800" _dark={{
+        color: "warmGray.50"
+      }} fontWeight="semibold">
+          Please fill out all 3 fields
+      </Heading>
+      <FormControl>
+            <FormControl.Label>Teacher: </FormControl.Label>
+            <Select placeholder = "Please Select"/>
+      </FormControl>
+      <FormControl>
+            <FormControl.Label>Class: </FormControl.Label>
+            <Select placeholder = "Please Select"/>
+      </FormControl>
+      <FormControl>
+            <FormControl.Label>Period: </FormControl.Label>
+            <Select placeholder = "Please Select"/>
+      </FormControl>
+      <Button mt="2">
+            Sign up
+      </Button>
+      <Button variant="outline" onPress={() => navigation.navigate('Calendar')}>
+        Back
+      </Button>
+      </VStack>
     </Box>
+  </Center>
   );
-}
+  // The dropdown menu will allow the user to choose there option for their class, teacher, and Period
+  // For the button once the user fills out what they will subscribe to, they have the option press subscribe to sumbit it 
+  // still need to figure out how to prevent a user from submitting less than 3 fields
+  //to-do: backend
+};
 
-export default CalendarScreen; 
+export default SubscribeMenu;
