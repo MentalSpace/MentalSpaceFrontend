9f6b5e0ae18277e0be18ce75510cec46af1ffe95
aditibordia <56941613+aditibordia@users.noreply.github.com>
2022-07-01 13:20:52 -0700

    Add files via upload

---
+++ src/screens/teacher_class.tsx
@@ -0,0 +1,55 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Box, Button, Center, Modal, Checkbox, FlatList, FormControl, Heading, Content, Icon, Input, VStack, Text} from "native-base";
+import { propsFlattener } from "native-base/lib/typescript/hooks/useThemeProps/propsFlattener";
+import React from "react";
+import { LoginStackList } from "../components/login_stack";
+
+type TeacherClassScreenProps = NativeStackScreenProps<LoginStackList, 'TeacherClass'>;
+
+function AddClass () {
+  prompt("Add a class.")
+}
+function ModifyClass () {
+  prompt("Edit this class.")
+}
+function DeleteClass () {
+  alert("Class deleted.")
+}
+
+function TeacherClassScreen({ navigation }: TeacherClassScreenProps) {
+    return (
+      <Center>
+        <div style = {{alignItems: "center"}}>
+        <br />
+          <Center> <Text style = {{fontSize: 41, textAlign: 'center', fontWeight: 'bold'}}>My Classes</Text></Center>
+            <br />
+            <br />
+            <Center>
+            <div>
+                    <Checkbox style = {{height: 25, width: 25}}><Text style = {{fontSize: 24}}>Period 1</Text></Checkbox>
+                    <Checkbox style = {{height: 25, width: 25}}><Text style = {{fontSize: 24}}>Period 2</Text></Checkbox>
+                    <Checkbox style = {{height: 25, width: 25}}><Text style = {{fontSize: 24}}>Period 3</Text></Checkbox>
+                    <Checkbox style = {{height: 25, width: 25}}><Text style = {{fontSize: 24}}>Period 4</Text></Checkbox>
+                    <Checkbox style = {{height: 25, width: 25}}><Text style = {{fontSize: 24}}>Period 5</Text></Checkbox>
+
+            </div>
+            </Center>
+            <br />
+            <br />
+            <Center>
+              <table>
+                <tr>
+                  <td><Button onPress = {AddClass} style = {{width: 100}}>Add </Button></td>
+              <br />
+              <td><Button onPress = {ModifyClass} style = {{width: 100}}>Edit</Button></td>
+              <br />
+              <td><Button onPress = {DeleteClass} style = {{width: 100}}>Delete</Button></td>
+              </tr>
+              </table>
+              </Center>
+        </div>
+        </Center>
+      );
+}
+
+export default TeacherClassScreen;
