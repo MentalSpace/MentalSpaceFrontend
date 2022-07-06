e6e905038eee79eee094d04254d0b16d0efc3e43
nixalpha <n1xalphaa@gmail.com>
2022-07-05 20:42:19 -0700

    Set up react query

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { extendTheme, NativeBaseProvider, theme } from 'native-base';
 import LoginStack from './src/components/login_stack';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const appTheme = extendTheme({
@@ -11,8 +14,10 @@ export default function App() {
   })
 
   return (
-    <NativeBaseProvider theme={appTheme}> 
-      <LoginStack />
-    </NativeBaseProvider>
+    <QueryClientProvider client={queryClient}>
+      <NativeBaseProvider theme={appTheme}> 
+        <LoginStack />
+      </NativeBaseProvider>
+    </QueryClientProvider>
   );
 }
