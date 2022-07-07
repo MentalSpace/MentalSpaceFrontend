8bb3828beb0da40440c3b4aa47fc6b365b9f6b27
niaverma05 <108018632+niaverma05@users.noreply.github.com>
2022-07-07 15:11:28 -0700

    Teacher Stack
    This is the menu that only teachers have access to. This menu will contain classes, students, etc.

---
+++ src/components/teacher_stack.tsx
@@ -0,0 +1,35 @@
+{/*
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from "react";
+// import ClassesScreen from ""
+// import AssignmentsScreen from "";
+// import StudentsInClassScreen from "";
+import TeacherSubscription from "../../screens/teacher_subscription";
+
+export type SideBarList = {
+    // Classes: undefined;
+    // Assignments: undefined;
+    TeacherSubscription: undefined;
+    // StudentsInClass: undefined;
+};
+
+const Drawer = createDrawerNavigator<SideBarList>();
+
+function TeacherStack () {
+  return (
+    <NavigationContainer independent={true}>
+      <Drawer.Navigator initialRouteName="TeacherSubscription">
+        {/*
+        <Drawer.Screen name = "Classes" component={ClassesScreen} />
+        <Drawer.Screen name = "Assignments" component={AssignmentsScreen} />
+         <Drawer.Screen name = "StudentsInClass" component={StudentsInClassScreen} />
+        <Drawer.Screen name = "TeacherSubscription" component={TeacherSubscription} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default TeacherStack;
+
+*/}
