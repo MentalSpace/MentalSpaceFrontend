cef4b49eae4b031a0f6fc40e924abc9162280281
nixalpha <n1xalphaa@gmail.com>
2022-07-08 13:42:42 -0700

    Add untested query hooks to work with tokens from the API

--- App.tsx
+++ App.tsx
@@ -3,7 +3,13 @@ import { extendTheme, NativeBaseProvider, theme } from 'native-base';
 import LoginStack from './src/components/login_stack';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity
+    }
+  }
+});
 
 export default function App() {
   const appTheme = extendTheme({
