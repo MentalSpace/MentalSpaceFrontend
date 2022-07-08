742c497e4d7ac995440367c273fb3b7a02b2f484
liv <108018871+llivys@users.noreply.github.com>
2022-07-08 14:14:15 -0700

    Connect teacher sign up to mock server

--- src/screens/teacher_signup.tsx
+++ src/screens/teacher_signup.tsx
@@ -1,11 +1,48 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Box, Button, Center, FormControl, Heading, Input, VStack } from "native-base";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { LoginStackList } from "../components/login_stack";
-
+import { apiUrl } from "../constants";
+ 
 type TeacherSignupProps = NativeStackScreenProps<LoginStackList, 'TeacherSignup'>;
-
+ 
+type RegisterTeacherResponse = {
+  status: string,
+};
+ 
 const TeacherSignup = ({navigation}: TeacherSignupProps) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [school, setSchool] = useState("");
+  const [department, setDepartment] = useState("");
+ 
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Prefer': 'code=200',
+      'X-CSRF-TOKEN': '123',
+      'Authorization': 'Bearer SOMETOKENVALUE'
+    },
+    body: JSON.stringify({
+       type: 'Teacher',
+       firstName: firstName,
+       lastName: lastName,
+       school: school,
+       department: department
+ 
+    })
+  };
+ 
+  const request = useQuery<RegisterTeacherResponse>('registerTeacher', async () => await (await fetch(apiUrl + '/teacher', requestOptions)).json(), {enabled: false});
+  useEffect(() => {
+    if (request.isSuccess) {
+      console.log(request.data.status);
+      if (request.data.status === "success") navigation.navigate('Login');
+    }
+  }, [request.isSuccess]);
+ 
   return <Center w="100%">
       <Box safeArea p="2" w="90%" maxW="290" py="8">
         <Heading size="lg" color="coolGray.800" _dark={{
@@ -21,18 +58,22 @@ const TeacherSignup = ({navigation}: TeacherSignupProps) => {
         <VStack space={3} mt="5">
         <FormControl>
             <FormControl.Label>First name</FormControl.Label>
-            <Input />
+            <Input value={firstName} onChangeText={setFirstName}/>
           </FormControl>
           <FormControl>
             <FormControl.Label>Last name</FormControl.Label>
-            <Input />
+            <Input value={lastName} onChangeText={setLastName}/>
           </FormControl>
           <FormControl>
             <FormControl.Label>School</FormControl.Label>
-            <Input />
+            <Input value={school} onChangeText={setSchool}/>
           </FormControl>
-          <Button mt="2" onPress={() => navigation.navigate('Home')}>
-            Sign up
+          <FormControl>
+            <FormControl.Label>Department</FormControl.Label>
+            <Input value={department} onChangeText={setDepartment}/>
+          </FormControl>
+          <Button mt="2" onPress={() => request.refetch()}>
+           Sign up
           </Button>
           <Button variant="outline" onPress={() => navigation.navigate('TeacherRegistration')}>
             Back
@@ -41,5 +82,6 @@ const TeacherSignup = ({navigation}: TeacherSignupProps) => {
       </Box>
     </Center>;
 }
-
+ 
 export default TeacherSignup;
+
