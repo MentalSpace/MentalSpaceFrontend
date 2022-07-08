7f692ad2654015fcedc1ba08e6f8d468b389275b
vinngo <104804772+vinngo@users.noreply.github.com>
2022-07-08 09:45:22 -0700

    Update signup_logic.tsx

--- src/signup_logic.tsx
+++ src/signup_logic.tsx
@@ -24,10 +24,34 @@ export const validateSame = (password: String, confirm: String) => { //logic to 
     }
   }
 
-export const canContinue = (email: String, password: String, confirm: String) => { //logic to validate if the user can continue. Returns if the user has filled all 3 fields correctly, returns false otherwise
+export const canContinue = (email: String, password: String, confirm: String) => { //logic to validate if the user can continue. Returns true if the user has filled all 3 fields correctly, returns false otherwise
     if (validateEmail(email) && validatePassword(password) && validateSame(password, confirm)){
       return true;
     } else {
       return false;
     }
   }
+
+export const validateString = (name: String) => { //logic to validate if the user has filled in a name field. Returns true if the user has filled in a field, false otherwise
+  if (!(name === "")){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const canContinueTeacher = (name: String, lastName: String, school: String) => { //logic to validate if a teacher has filled all fields required
+  if (validateString(name) && validateString(lastName) && validateString(school)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const canContinueStudent = (name: String, lastName: String, studentId: String, school: String) => { //logic to validate if a student has filled all fields required
+  if (validateString(name) && validateString(lastName) && validateString(school) && validateString(studentId)){
+    return true;
+  } else {
+    return false;
+  }
+}
