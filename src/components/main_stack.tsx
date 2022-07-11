78b38f76332726cc297706903e2e89caedd9f093
rahulmohari <56941594+rahulmohari@users.noreply.github.com>
2022-07-11 13:45:05 -0700

    profile button
    + sign out button + dark mode switch

--- src/components/main_stack.tsx
+++ src/components/main_stack.tsx
@@ -1,16 +1,16 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Menu, Pressable, Avatar, Switch } from 'native-base';
 import React from 'react';
 
+import { LoginStackList } from "../components/login_stack";
 import AddClassScreen from '../screens/add_class_screen';
 import AddNonHWScreen from '../screens/add_non_hw_screen';
 import CalendarScreen from '../screens/calendar_screen';
 import TaskListScreen from '../screens/student_tasklist';
 import AssignmentsScreen from '../screens/teacher_assignment_screen';
 
-import {Menu, Pressable, Avatar, Switch } from "native-base";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { LoginStackList } from "../components/login_stack";
 
 export type SideBarList = {
   Calendar: undefined;
@@ -18,12 +18,11 @@ export type SideBarList = {
   Task: undefined;
   AddNonHW: undefined;
   Assignments: undefined;
-};
+=};
 
 type ProfileProps = NativeStackScreenProps<LoginStackList, 'SignOut'>;
 const Drawer = createDrawerNavigator<SideBarList>();
-
-function MainStack({navigation}:ProfileProps) {
+function MainStack({ navigation }: ProfileProps) {
   return (
     <NavigationContainer independent>
       <Drawer.Navigator initialRouteName="Calendar">
@@ -34,29 +33,43 @@ function MainStack({navigation}:ProfileProps) {
         <Drawer.Screen name="Assignments" component={AssignmentsScreen} />
       </Drawer.Navigator>
       
-      <Menu trigger={triggerProps => {
-    return (
-      <Pressable 
-        position='absolute' 
-        height='60' 
-        width='60' 
-        top='0.5 '
-        right= '2'
-        bottom='5'
-        justifyContent='center'
-        alignItems='center'
-        accessibilityLabel='More options menu' {...triggerProps}
+      <Menu
+        trigger={(triggerProps) => {
+          return (
+            <Pressable
+              position="absolute"
+              height="60"
+              width="60"
+              top="0.5 "
+              right="2"
+              bottom="5"
+              justifyContent="center"
+              alignItems="center"
+              accessibilityLabel="More options menu"
+              {...triggerProps}
+            >
+              <Avatar
+                bg="#1697b7"
+                source={{ uri: 'https://bit.ly/broken-link' }}
+              >
+                RM
+              </Avatar>
+            </Pressable>
+          );
+        }}
       >
-          <Avatar bg="#1697b7" source={{uri: "https://bit.ly/broken-link"}}>
-          RM
-          </Avatar>
-      </Pressable>);
-  }}>
-    <Menu.Item onPress={() => navigation.navigate('Login')}>
-      Sign Out
-    </Menu.Item>
-    <Menu.Item><Switch offTrackColor="orange.100" onTrackColor="light.800" onThumbColor="light.900" offThumbColor="orange.50" /></Menu.Item>
-  </Menu>
+        <Menu.Item onPress={() => navigation.navigate('Login')}>
+          Sign Out
+        </Menu.Item>
+        <Menu.Item>
+          <Switch
+            offTrackColor="orange.100"
+            onTrackColor="light.800"
+            onThumbColor="light.900"
+            offThumbColor="orange.50"
+          />
+        </Menu.Item>
+      </Menu>
     </NavigationContainer>
   );
 }
