a745d91a1cda06afbc500573e99f6eccba281cd8
Aathi Muthu <aathijmuthu@gmail.com>
2022-07-11 09:30:28 -0700

    Add teacher assignment screen. Still need to clear text textboxes when assignment is added, and still need to test with mock server.

--- src/components/main_stack.tsx
+++ src/components/main_stack.tsx
@@ -1,29 +1,34 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-
-import AddClassScreen from '../screens/add_class_screen';
-import AddNonHWScreen from '../screens/add_non_hw_screen';
-import CalendarScreen from '../screens/calendar_screen';
-import TaskListScreen from '../screens/student_tasklist';
+import React from "react";
+import CalendarScreen from "../screens/calendar_screen";
+import AddClassScreen from "../screens/add_class_screen";
+import TaskListScreen from "../screens/student_tasklist";
+import AddNonHWScreen from "../screens/add_non_hw_screen";
+import AssignmentsScreen from "../screens/teacher_assignment_screen";
+import TeacherSubscription from "../screens/teacher_subscription";
 
 export type SideBarList = {
-  Calendar: undefined;
-  AddClass: undefined;
-  Task: undefined;
-  AddNonHW: undefined;
+    Calendar: undefined;
+    AddClass: undefined;
+    Task: undefined;
+    AddNonHW: undefined
+    Assignments: undefined;
+    TeachSubscript: undefined;
 };
 
 const Drawer = createDrawerNavigator<SideBarList>();
 
 function MainStack() {
   return (
-    <NavigationContainer independent>
+    <NavigationContainer independent={true}>
       <Drawer.Navigator initialRouteName="Calendar">
         <Drawer.Screen name="Calendar" component={CalendarScreen} />
         <Drawer.Screen name="AddClass" component={AddClassScreen} />
         <Drawer.Screen name="Task" component={TaskListScreen} />
         <Drawer.Screen name="AddNonHW" component={AddNonHWScreen} />
+        <Drawer.Screen name="Assignments" component={AssignmentsScreen} />
+        <Drawer.Screen name="TeachSubscript" component={TeacherSubscription} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
