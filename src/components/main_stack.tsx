d12ec5ec2814b42d531997245e6bde6f52bd324c
aathijmuthu <65832367+aathijmuthu@users.noreply.github.com>
2022-07-11 13:16:57 -0700

    Add Teacher Assignment Screen (#28)
    * Add teacher assignment screen. Still need to
    clear text textboxes when assignment is added, and still need to
    test with mock server.
    
    * Bug mix in main stack
    
    * Bug fix
    
    * Format using prettier and add code to clear input
    
    * Ran eslint
    
    * Deleted package lock

--- src/components/main_stack.tsx
+++ src/components/main_stack.tsx
@@ -6,12 +6,14 @@ import AddClassScreen from '../screens/add_class_screen';
 import AddNonHWScreen from '../screens/add_non_hw_screen';
 import CalendarScreen from '../screens/calendar_screen';
 import TaskListScreen from '../screens/student_tasklist';
+import AssignmentsScreen from '../screens/teacher_assignment_screen';
 
 export type SideBarList = {
   Calendar: undefined;
   AddClass: undefined;
   Task: undefined;
   AddNonHW: undefined;
+  Assignments: undefined;
 };
 
 const Drawer = createDrawerNavigator<SideBarList>();
@@ -24,6 +26,7 @@ function MainStack() {
         <Drawer.Screen name="AddClass" component={AddClassScreen} />
         <Drawer.Screen name="Task" component={TaskListScreen} />
         <Drawer.Screen name="AddNonHW" component={AddNonHWScreen} />
+        <Drawer.Screen name="Assignments" component={AssignmentsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
