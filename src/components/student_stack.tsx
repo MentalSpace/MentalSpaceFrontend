0c2a0bf8ed2f7ca03144e71e2e06df3bd6e4308a
vinngo <104804772+vinngo@users.noreply.github.com>
2022-07-13 09:17:58 -0700

    implements student menu into student stack.
    student menu will be the first thing students see when signing in

--- src/components/student_stack.tsx
+++ src/components/student_stack.tsx
@@ -6,9 +6,11 @@ import CalendarScreen from '../screens/calendar_screen';
 import AddClassScreen from '../screens/student/add_class_screen';
 import AddNonHWScreen from '../screens/student/add_non_hw_screen';
 import ScheduleOptionsScreen from '../screens/student/schedule_options';
+import StudentMenu from '../screens/student/student_menu';
 import TaskListScreen from '../screens/student/student_tasklist';
 
 export type SideBarList = {
+  Menu: undefined;
   Calendar: undefined;
   AddClass: undefined;
   Task: undefined;
@@ -22,7 +24,8 @@ const Drawer = createDrawerNavigator<SideBarList>();
 function StudentStack() {
   return (
     <NavigationContainer independent>
-      <Drawer.Navigator initialRouteName="Calendar">
+      <Drawer.Navigator initialRouteName="Menu">
+        <Drawer.Screen name="Menu" component={StudentMenu} />
         <Drawer.Screen name="Calendar" component={CalendarScreen} />
         <Drawer.Screen name="AddClass" component={AddClassScreen} />
         <Drawer.Screen name="Task" component={TaskListScreen} />
