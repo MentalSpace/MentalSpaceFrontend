6cb248aaadb07bba7712a50b25b4d5b40a7c530f
vinngo <104804772+vinngo@users.noreply.github.com>
2022-07-13 10:51:53 -0700

    Update add_class_screen.tsx

--- src/screens/student/add_class_screen.tsx
+++ src/screens/student/add_class_screen.tsx
@@ -7,15 +7,50 @@ import {
   Heading,
   Input,
   VStack,
-  View,
 } from 'native-base';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 import { SideBarList } from '../../components/student_stack';
+import { apiUrl } from '../../constants';
+import { validateString } from '../../signup_logic';
 
 type AddClassScreenProps = NativeStackScreenProps<SideBarList, 'AddClass'>;
 
+type RegisterUserResponse = {
+  status: string;
+};
+
 function AddClassScreen({ navigation }: AddClassScreenProps) {
+  const [classCode, setClassCode] = useState('');
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Prefer: 'code=200',
+      'X-CSRF-TOKEN': '123',
+    },
+    body: JSON.stringify({
+      type: 'Student',
+      classCode,
+    }),
+  };
+  const request = useQuery<RegisterUserResponse>(
+    'registerUser',
+    async () =>
+      await (await fetch(apiUrl + '/class/join', requestOptions)).json(),
+    { enabled: false }
+  );
+  useEffect(() => {
+    if (request.isSuccess) {
+      console.log(request.data.status);
+      if (request.data.status === 'success') {
+        navigation.navigate('Menu');
+      }
+    }
+  }, [request.isSuccess]);
+
   return (
     <Center w="100%">
       <Box safeArea p="2" py="8" w="90%" maxW="290">
@@ -30,10 +65,26 @@ function AddClassScreen({ navigation }: AddClassScreenProps) {
           >
             Enter a Class Code
           </Heading>
-          <Input placeholder="Class Code" />
-          <Button>Enter Code</Button>
-          <Button onPress={() => navigation.navigate('Calendar')}>
-            Go back to the calendar
+          <FormControl>
+            <Input
+              placeholder="Class Code"
+              value={classCode}
+              onChangeText={setClassCode}
+            />
+            <FormControl.HelperText>
+              {validateString(classCode) ? '' : 'Please enter a class code'}
+            </FormControl.HelperText>
+          </FormControl>
+          <Button
+            disabled={!validateString(classCode)}
+            onPress={() => {
+              request.refetch();
+            }}
+          >
+            Enter Code
+          </Button>
+          <Button onPress={() => navigation.navigate('Menu')}>
+            Go back to the main menu
           </Button>
         </VStack>
       </Box>
