a48b6146f8573a6b60d7a11f0b78adcd85e0c889
p0tato <semverkerk.1104@gmail.com>
2022-07-13 13:45:20 -0700

    teacher_class broken screen commented out

--- src/components/teacher_stack.tsx
+++ src/components/teacher_stack.tsx
@@ -3,14 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import CalendarScreen from '../screens/calendar_screen';
 // import AssignmentsScreen from '../screens/teacher/teacher_assignment_screen';
-import TeacherClassScreen from '../screens/teacher/teacher_class';
+//import TeacherClassScreen from '../screens/teacher/teacher_class';
 //import EditAssignmentsScreen from '../screens/teacher/teacher_edit_assignment_screen';
 
 export type SideBarList = {
   Calendar: undefined;
   //Assignments: undefined;
   //EditAssignments: undefined;
-  TeacherClass: undefined;
+  //TeacherClass: undefined;
 };
 
 const Drawer = createDrawerNavigator<SideBarList>();
@@ -25,7 +25,7 @@ function StudentStack() {
           name="EditAssignments"
           component={EditAssignmentsScreen}
         /> */}
-        <Drawer.Screen name="TeacherClass" component={TeacherClassScreen} />
+        {/* <Drawer.Screen name="TeacherClass" component={TeacherClassScreen} /> */}
       </Drawer.Navigator>
     </NavigationContainer>
   );
