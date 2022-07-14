a32c08515c2a3abd9def35d5f112d5bc5f6e754b
Keen Do <73369596+BottledSheep@users.noreply.github.com>
2022-07-14 10:40:31 -0700

    bearer token for request options

--- src/screens/student/schedule_options.tsx
+++ src/screens/student/schedule_options.tsx
@@ -42,10 +42,12 @@ const ScheduleOptionsScreen = ({ navigation }: ScheduleOptionsProps) => {
   const [preferenceId, setPreferenceId] = useState(0);
 
   const csrfToken = useCSRFToken();
+  const accessToken = useAccessToken();
 
   const requestOptions = {
     method: 'PATCH',
     headers: {
+      Authorization: 'Bearer ' + accessToken.data!.accessToken,
       'Content-Type': 'application/json',
       Prefer: 'code=200',
       'X-CSRF-TOKEN': csrfToken.data!.csrfToken,
