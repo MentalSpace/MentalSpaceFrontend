e99c804019e6c556d41436b5989bb0c8bcf3fc11
Aathi Muthu <aathijmuthu@gmail.com>
2022-07-15 09:45:52 -0700

    API url again

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,1 @@
-export const apiUrl =
-  'http://localhost:19006/api/v0'; //"https://dev-waffle-cone.probablyanasian.dev"
+export const apiUrl = 'http://localhost:19006/api/v0'; //"https://dev-waffle-cone.probablyanasian.dev"
