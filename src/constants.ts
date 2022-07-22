2e8efd652588fca035201d6e10c7472e14a03585
nixalpha <n1xalphaa@gmail.com>
2022-07-22 10:48:57 -0700

    Update apiUrl for platform specific differences

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const apiUrl = 'http://localhost:19006/api/v0'; //"https://dev-waffle-cone.probablyanasian.dev"
+import { Platform } from "react-native";
+
+export const apiUrl = Platform.OS === 'web' ? 'http://localhost:19006/api/v0' : 'http://10.33.110.91:8039/api/v0';
 
 export enum AccountType {
   Student = 'Student',
