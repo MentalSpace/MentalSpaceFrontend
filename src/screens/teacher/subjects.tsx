9a21e399008e237bb3f67322d0eb73ff346c3592
diegos9583 <diegos9583@gmail.com>
2022-07-22 14:20:49 -0700

    Changed to Fake Subject Screen

--- src/screens/teacher/subjects.tsx
+++ src/screens/teacher/subjects.tsx
@@ -1,4 +1,5 @@
-// import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { SideBarList } from '../../components/teacher_stack';
 import {
   Button,
   Center,
@@ -10,156 +11,93 @@ import {
   Select,
   CheckIcon,
   Box,
+  VStack,
+  Text,
 } from 'native-base';
 // import { useQuery, useQueryClient } from 'react-query';
 import React, { ClassType, Component, useEffect, useState } from 'react';
 import { Alert, StyleSheet, TextInput } from 'react-native';
-import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
+// import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
-const styles = StyleSheet.create({
-  text: { textAlign: 'center' },
-  btnText: { textAlign: 'center' },
-  container: {
-    flex: 1,
-    padding: 16,
-    paddingTop: 30,
-    backgroundColor: 'gray.800',
-  },
-  head: { height: 40, backgroundColor: 'amber.200' },
-  row: { flexDirection: 'row', backgroundColor: 'gray.800' },
-  btn: { width: 28, height: 18, backgroundColor: 'gray.800', borderRadius: 2 },
-});
+type ClassDisplayProps = NativeStackScreenProps<SideBarList, 'Subjects'>;
 
-const stylesPT2 = StyleSheet.create({
-  input: {
-    height: 30,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: 100,
-    borderRadius: 4,
-  },
-});
-
-const stylesPT3 = StyleSheet.create({
-  input: {
-    height: 30,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: 200,
-    borderRadius: 4,
-  },
-});
-
-const SubjectSelect = () => {
-  let [service, setService] = React.useState('');
+function SubjectsScreen({ navigation }: ClassDisplayProps) {
   return (
-    <Box maxW="200">
-      <Select
-        selectedValue={service}
-        minWidth="20"
-        maxWidth="1000"
-        accessibilityLabel="Select"
-        placeholder="Select"
-        _selectedItem={{
-          bg: 'orange',
-          paddingRight: '10',
-          endIcon: <CheckIcon size="2" />,
-        }}
-        mt={1}
-        onValueChange={(itemValue: any) => setService(itemValue)}
-      >
-        <Select.Item label="APCSA" value="APCSA" />
-        <Select.Item label="Calc" value="Calc" />
-      </Select>
-    </Box>
+    <Center>
+      <Box>
+        <Heading
+          size="lg"
+          color="coolGray.800"
+          _dark={{ color: 'warmGray.50' }}
+          fontWeight="semibold"
+          paddingTop="5"
+          paddingBottom="30"
+        >
+          Subjects
+        </Heading>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Math 1
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Math 2
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Math 3
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Math Analysis
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          MRWC
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          AP Calc AB
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          AP Calc BC
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" mb="5" fontSize={'18'}>
+          AP Statistics
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          English 1
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          English 1A
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          English 2
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          English 2A
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          English 3
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          AP Lang
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          ERWC
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          AP Lit
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Genres of Comp
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Journalism
+        </Text>
+        <Text color="coolGray.800" fontWeight="semibold" fontSize={'18'}>
+          Gothic Lit
+        </Text>
+        <Button mt={7} size="xs" style={{ borderRadius: 30 }} maxWidth="39">
+          <AddIcon size="5" mt="0.5" color="white" />
+        </Button>
+      </Box>
+    </Center>
   );
-};
-
-export default class Subjects extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      tableHead: ['', 'Subjects', '', 'Description'],
-      tableData: [
-        ['', <SubjectSelect />, '', <TextInput style={stylesPT3.input} />],
-        ['', <SubjectSelect />, '', <TextInput style={stylesPT3.input} />],
-        ['', <SubjectSelect />, '', <TextInput style={stylesPT3.input} />],
-        ['', <SubjectSelect />, '', <TextInput style={stylesPT3.input} />],
-      ],
-    };
-  }
-
-  _alertIndex(index: any) {
-    Alert.alert(`This is row ${index + 1}`);
-  }
-
-  render() {
-    const state = this.state;
-    const element = (data: any, index: any) => (
-      <Checkbox
-        value="true"
-        onChange={() => this._alertIndex(index)}
-      ></Checkbox>
-    );
-
-    return (
-      <View style={styles.container}>
-        <Center>
-          <Heading
-            size="xl"
-            color="coolGray.800"
-            _dark={{
-              color: 'warmGray.50',
-            }}
-            fontWeight="semibold"
-            paddingBottom="30"
-            paddingTop="2%"
-          >
-            Subjects
-          </Heading>
-        </Center>
-        <Center>
-          <Table borderStyle={{ borderColor: 'transparent' }}>
-            <Row
-              data={state.tableHead}
-              style={styles.head}
-              textStyle={styles.text}
-            />
-            {state.tableData.map((rowData: any, index: any) => (
-              <TableWrapper key={index} style={styles.row}>
-                {rowData.map((cellData: any, cellIndex: any) => (
-                  <Cell
-                    key={cellIndex}
-                    data={cellIndex === 0 ? element(cellData, index) : cellData}
-                    textStyle={styles.text}
-                  />
-                ))}
-              </TableWrapper>
-            ))}
-          </Table>
-
-          <Center>
-            <View
-              style={{
-                alignSelf: 'stretch',
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <Button
-                style={{ marginTop: 30, marginRight: 21, borderRadius: 30 }}
-              >
-                <AddIcon size="5" mt="0.5" color="white" />
-              </Button>
-              <Button style={{ marginTop: 30, borderRadius: 30 }}>
-                <DeleteIcon size="5" mt="0.5" color="white" />
-              </Button>
-            </View>
-          </Center>
-        </Center>
-      </View>
-    );
-  }
 }
+
+export default SubjectsScreen;
